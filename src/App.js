d09d5dfbd1874f7e0c4a448def426fd1f6fcbe18
Pablo Jose Calle <pjcb1998@hotmail.com>
2021-08-17 11:26:10 -0500

    Project Creation

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+
+function App() {
+  return (
+            <ChatEngine
+                height="100vh"
+                projectID="
+                1db65471-ab4a-4d9f-8a77-4ff550fd067b"
+                userName="Test"
+                userSecret="123456"
+            />
+  );
+}
+
+export default App;
