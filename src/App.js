f77218feff5470d47074a491577932b45460ff87
Edwin M <andresedwinmg14@gmail.com>
2021-09-20 21:15:49 -0500

    Pantalla de información
    Hay una carpeta que se llama react-website-1 donde estan los elementos de la página donde se tendra la información con respecto al suicidio; Sin embargo aún no esta conectada con el resto de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-import './App.css';
-import LoginForm from './components/LoginForm';
-import { ChatEngine } from 'react-chat-engine';
-
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+import { ChatEngine } from "react-chat-engine";
 
 function App() {
-  if(!localStorage.getItem('username')) return <LoginForm></LoginForm>
+  if (!localStorage.getItem("username")) return <LoginForm></LoginForm>;
   return (
-            <ChatEngine
-                height="100vh"
-                projectID="1db65471-ab4a-4d9f-8a77-4ff550fd067b"
-                userName={localStorage.getItem('username')}
-                userSecret={localStorage.getItem('password')}
-            />
+    <ChatEngine
+      height="100vh"
+      projectID="1db65471-ab4a-4d9f-8a77-4ff550fd067b"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+    />
   );
 }
-
 export default App;
